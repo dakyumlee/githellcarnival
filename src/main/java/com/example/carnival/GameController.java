5146bc725345400da1be4a8ca4479cd4f5cee4bc
dakyumlee <oicrcutie@gmail.com>
2025-09-16 20:04:57 +0900

    cherryPICK

--- src/main/java/com/example/carnival/GameController.java
+++ src/main/java/com/example/carnival/GameController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GameController {
-
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("message", "🎮 Welcome to Git Hell Carnival!");
