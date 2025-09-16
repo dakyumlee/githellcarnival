0d94b5ae5959c37bc0bcef0e196ee4cf2d033183
dakyumlee <oicrcutie@gmail.com>
2025-09-16 23:51:51 +0900

    Fix root path mapping

--- src/main/java/com/example/carnival/GameController.java
+++ src/main/java/com/example/carnival/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GameController {
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("message", "🎮 Welcome to Git Hell Carnival!");
         return "index";
     }
 }
