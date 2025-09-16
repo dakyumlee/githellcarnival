832be66dbd1db3654e073f100810d8a70b1fd685
dakyumlee <oicrcutie@gmail.com>
2025-09-16 17:35:45 +0900

    뼈대

---
+++ src/main/java/com/example/carnival/GitHellCarnivalApplication.java
@@ -0,0 +1,11 @@
+package com.example.carnival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHellCarnivalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHellCarnivalApplication.class, args);
+    }
+}
